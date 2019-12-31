3eb47010e5659060fcca5e89604fbdad1dea92ac
EC2 Default User <ec2-user@ip-172-31-29-34.us-east-2.compute.internal>
2019-12-31 16:01:03 +0000

    edit gitignore, volume for db was created, edit script, yml was fixed with new command for mysql

--- docker.sh
+++ docker.sh
@@ -3,7 +3,5 @@ echo "Pass you password"
 read -s $REPLY
 echo "PASSWORD=$REPLY" > .env
 docker rm -f nginx mysql php-fpm
-docker-compose build --no-cache 
+docker-compose build  
 docker-compose up -d --force-recreate
-echo $REPLY
-docker exec -it mysql mysql -uroot -p`echo $REPLY` -e "alter user 'root'@'%' identified with mysql_native_password by '${REPLY}';"
